5fa35c1c99520eb14cb758c5cc43e3d5a30e03c1
jspe2ba12 <jspe2ba12@scs.local>
2025-04-02 10:42:29 +0800

    School work

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http'
+import axios from 'axios';
+
 const API_KEY = environment.API_KEY;
 const API_URL = environment.API_URL;
-
 
 @Component({
   selector: 'app-home',
@@ -12,24 +13,47 @@ const API_URL = environment.API_URL;
   standalone: false,
 })
 export class HomePage {
-
   todayDate = new Date();
   weatherTemp: any;
-  constructor(public httpClient:HttpClient) {
-    this.loadData();
-  }
-  loadData() {
-    this.httpClient.get(`${API_URL}/weather?q=${"Cebu"}&appid=${API_KEY}`).subscribe({
-      next: (results) => {
-        console.log(results);
-        this.weatherTemp = results;
-        console.log(this.weatherTemp);
-      },
-      error: (err) => {
-        console.error('Error fetching weather data:', err);
-      }
-    });
+
+  cityInput: string = '';
+  
+  async getCityLocation(): Promise<void>{
+  const cityName = this.cityInput.trim();
+  const value = '';
+
+    if(cityName === ''){
+      console.log("There is no City");
+      return;    
+    }
 
     
+
+   let GEOCODING_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=$limit={limit}$appid={API_KEY}`;
+
+
+    }
   }
-}
+
+
+
+
+
+  // constructor(public httpClient:HttpClient) {
+  //   this.loadData();
+  // }
+  // loadData() {
+  //   this.httpClient.get(`${API_URL}/weather?q=${"cityName"}&appid=${API_KEY}`).subscribe({
+  //     next: (results) => {
+  //       console.log(results);
+  //       this.weatherTemp = results;
+  //       console.log(this.weatherTemp);
+  //     },
+  //     error: (err) => {
+  //       console.error('Error fetching weather data:', err);
+  //     }
+  //   });
+
+    
+//   }
+// }
